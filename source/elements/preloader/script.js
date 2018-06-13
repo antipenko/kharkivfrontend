938dc0fc6e16b6ffcc9e7e348e57808aa907fbfe
antipenko <sweetglory.victory@gmail.com>
2018-06-13 22:57:45 +0300

    добавляет информацию о KharkivFrontend Summer Conference

--- source/elements/preloader/script.js
+++ source/elements/preloader/script.js
@@ -1,12 +1,12 @@
 'use strict';
 (function () {
-	var $preloader = $('.preloader');
-
-	//fade-preloader
-	$(window).on("load", function () {
-		$preloader.delay(1500).fadeOut(1500, function(){
-			new WOW().init();
-		});
-	});
+	// var $preloader = $('.preloader');
+    //
+	// //fade-preloader
+	// $(window).on("load", function () {
+	// 	$preloader.delay(1500).fadeOut(1500, function(){
+	// 		new WOW().init();
+	// 	});
+	// });
 
 })();
